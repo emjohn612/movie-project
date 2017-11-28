6152a1352a3fa9c28c0bf14035b8a002a3d1ad4e
Eric M Johnston <emjohn612@gmail.com>
2017-11-27 19:19:42 -0500

    formatting

--- entertainment_center.py
+++ entertainment_center.py
@@ -10,7 +10,7 @@ the_road = media.Movie("The Road",
                        "In a dangerous post-apocalyptic world,\
                         an ailing father defends his son as they slowly\
                         travel to the sea."
-                       "https://images-na.ssl-images-amazon.com/images/M/MV5BMTAwNzk4NTU3NDReQTJeQWpwZ15BbWU3MDg3OTEyODI@._V1_UY268_CR0,0,182,268_AL_.jpg",
+                       "https://images-na.ssl-images-amazon.com/images/M/MV5BMTAwNzk4NTU3NDReQTJeQWpwZ15BbWU3MDg3OTEyODI@._V1_UY268_CR0,0,182,268_AL_.jpg",  # noqa
                        "https://www.youtube.com/watch?v=FsFFwaRxoDw")
 
 into_the_wild = media.Movie("Into the Wild",
@@ -18,21 +18,21 @@ into_the_wild = media.Movie("Into the Wild",
                              top student and athlete Christopher McCandless\
                              abandons his possessions, and hitchhikes to\
                              Alaska to live in the wilderness.",
-                            "https://upload.wikimedia.org/wikipedia/en/8/8a/Into-the-wild.jpg",
+                            "https://upload.wikimedia.org/wikipedia/en/8/8a/Into-the-wild.jpg",  # noqa
                             "https://www.youtube.com/watch?v=2LAuzT_x8Ek")
 
 the_incredibles = media.Movie("The Incredibles",
                               "A family of undercover superheroes, while\
                                trying to live the quiet suburban life, are\
                                forced into action to save the world.",
-                              "https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-12561-18hpovx_f8238c31.jpeg?region=0,0,300,450",
+                              "https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-12561-18hpovx_f8238c31.jpeg?region=0,0,300,450",  # noqa
                               "https://www.youtube.com/watch?v=eZbzbC9285I")
 
 man_on_fire = media.Movie("Man On Fire",
                           "In Mexico City, a former assassin swears vengeance\
                            on those who committed an unspeakable act against \
                            the family he was hired to protect.",
-                          "https://images-na.ssl-images-amazon.com/images/M/MV5BODFlMmEwMDgtYjhmZi00ZTE5LTk2NWQtMWE1Y2M0NjkzOGYxXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY268_CR0,0,182,268_AL_.jpg",
+                          "https://images-na.ssl-images-amazon.com/images/M/MV5BODFlMmEwMDgtYjhmZi00ZTE5LTk2NWQtMWE1Y2M0NjkzOGYxXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY268_CR0,0,182,268_AL_.jpg",  # noqa
                           "https://www.youtube.com/watch?v=rb-ZEBBKolc")
 
 how_i_live_now = media.Movie("How I Live Now",
@@ -40,7 +40,7 @@ how_i_live_now = media.Movie("How I Live Now",
                               to stay with relatives, finds love and purpose\
                               while fighting for her survival as war envelops\
                               the world around her.",
-                             "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU4NTg4NzgzMF5BMl5BanBnXkFtZTgwOTU1NTMxMDE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+                             "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU4NTg4NzgzMF5BMl5BanBnXkFtZTgwOTU1NTMxMDE@._V1_UX182_CR0,0,182,268_AL_.jpg",  # noqa
                              "https://www.youtube.com/watch?v=RSaxm68PPT4")
 
 hanna = media.Movie("Hanna",
@@ -48,7 +48,7 @@ hanna = media.Movie("Hanna",
                      to be the perfect assassin is dispatched on a mission\
                      across Europe, tracked by a ruthless intelligence agent\
                      and her operatives.",
-                    "https://images-na.ssl-images-amazon.com/images/M/MV5BNTAzMTg1NjY0NF5BMl5BanBnXkFtZTcwODc3MTgzNA@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+                    "https://images-na.ssl-images-amazon.com/images/M/MV5BNTAzMTg1NjY0NF5BMl5BanBnXkFtZTcwODc3MTgzNA@@._V1_UX182_CR0,0,182,268_AL_.jpg",  # noqa
                     "https://www.youtube.com/watch?v=u73CLdHpbNk")
 
 """
